90ab60d662d2d2cb5d0d6b89148ce157c1d12819
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-25 01:18:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,11 +7,11 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
+  elsif string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
-  else
-    return "I LOVE YOU TOO PUMPKIN!"
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
   end
